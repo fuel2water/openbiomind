838bc17f6e1d005aaf2c4d1d9f8cf3b773b42667
lucioc <lucioc@c415a1d6-9c3f-0410-908e-bdf2f35a6748>
2007-11-23 22:32:19 +0000

    New reset

--- trunk/openbiomind/classification/evolution/EvolvableFactory.java
+++
@@ -1,9 +0,0 @@
-package classification.evolution;
-
-public interface EvolvableFactory{
-
-       public Evolvable makeEvolvable();
-
-       public Evolvable makeEvolvable(Evolvable mom,Evolvable dad);
-       
-}
