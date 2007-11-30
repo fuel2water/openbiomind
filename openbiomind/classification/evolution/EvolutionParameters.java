cc333082b1a7cddeb8ffe3b8e145e092ee7fdbcd
lucioc <lucioc@c415a1d6-9c3f-0410-908e-bdf2f35a6748>
2007-11-30 22:21:13 +0000

    A few bug fixes and improvements after "alfa" SNPs integration.

--- openbiomind/classification/evolution/EvolutionParameters.java
+++ openbiomind/classification/evolution/EvolutionParameters.java
@@ -5,7 +5,7 @@ public class EvolutionParameters{
        private EvolvableFactory evolvableFactory;
        private int maxGenerations=50;
        private int tournamentSize=2;
-       private int populationSize=100;
+       private int populationSize=1000;
        private FitnessEvaluator fitnessEvaluator;
 
        public EvolutionParameters(EvolvableFactory evolvableFactory,FitnessEvaluator fitnessEvaluator){
